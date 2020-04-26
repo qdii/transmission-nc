8f81e02c727610a2c013c9dac031a3c86467adcf
Victor Lavaud <victor.lavaud@gmail.com>
2020-04-26 11:01:52 +0200

    Added almost empty Admin.php page to store settings

---
+++ transmission/lib/Settings/Admin.php
@@ -0,0 +1,32 @@
+<?php
+
+use OCP\IL10N;
+use OCP\Settings\ISettings;
+
+class Admin implements ISettings {
+	/** @var IL10N */
+	private $l;
+
+	/**
+	 * @param IL10N $l
+	 */
+	public function __construct(IL10N $l) {
+		$this->l = $l;
+	}
+
+	/**
+	 * @return TemplateResponse
+	 */
+	public function getForm() {
+        $parameters = [];
+		return new TemplateResponse('transmission', 'settings', $parameters);
+	}
+
+	public function getSection() {
+		return 'server';
+	}
+
+	public function getPriority() {
+		return 100;
+	}
+}
