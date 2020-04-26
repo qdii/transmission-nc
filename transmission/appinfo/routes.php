62b571c4b8b73a765a27cd2d63a8e186ae02f555
Victor Lavaud <victor.lavaud@gmail.com>
2020-04-26 19:42:18 +0200

    added SettingsController

--- transmission/appinfo/routes.php
+++ transmission/appinfo/routes.php
@@ -3,5 +3,6 @@ return [
     'routes' => [
 	   ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
 	   ['name' => 'transmission#rpc', 'url' => '/rpc', 'verb' => 'POST'],
+	   ['name' => 'settings#save', 'url' => '/save', 'verb' => 'POST'],
     ]
 ];
