b0d1676c399636f5a931ffa60b7902784b4af676
Victor Lavaud <victor.lavaud@gmail.com>
2021-01-12 23:15:21 +0100

    Added a HTML form to set the RPC port

--- transmission/templates/settings.php
+++ transmission/templates/settings.php
@@ -1,2 +1,7 @@
-<h2>Transmission</h2>
-Hello World!
+<div id="transmission" class="section">
+  <h2>Transmission</h2>
+  <form id="transmission-settings-form">
+    <label for="transmission-rpc-port-input">RPC Port:</label>
+    <input id="transmission-rpc-port-input" type="text"></input>
+  </form>
+</div>
